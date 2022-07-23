d89e97e3a90669645268b91b1abc4414e64cf2de
brittbala1 <109651689+brittbala1@users.noreply.github.com>
2022-07-23 12:05:49 +0530

    Add files via upload

---
+++ environment/deploy.ps1
@@ -0,0 +1,31 @@
+$resourceGroupName = "Test123"            # <-- REPLACE the variable values with your own values.
+$location = "australiasoutheast"                 # <-- Ensure that the location is a valid Azure location
+$storageAccountName = "Testfunctionsdev007"           # <-- Ensure the storage account name is unique
+$appServicePlanName = "AustraliaSoutheastPlans"   # <--
+$appInsightsName = "axyfunctionsdev"              # <--
+$functionName = "axyzfunctionsdev"                 # <--
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Force -Verbose
+
+$params = @{
+    storageAccountName = $storageAccountName.ToLower()
+    appServicePlanName = $appServicePlanName
+    appInsightsName    = $appInsightsName
+    functionName       = $functionName
+}
+
+$output = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterObject $params -Verbose
+
+New-AzRoleAssignment -RoleDefinitionName "Reader" -ObjectId $output.Outputs.managedIdentityId.Value -ErrorAction SilentlyContinue -Verbose
+New-AzRoleAssignment -RoleDefinitionName "Tag Contributor" -ObjectId $output.Outputs.managedIdentityId.Value -ErrorAction SilentlyContinue -Verbose
+
+Push-Location
+Set-Location ..\functions
+Compress-Archive -Path * -DestinationPath ..\environment\functions.zip -Force -Verbose
+Pop-Location
+
+$file = (Get-ChildItem .\functions.zip).FullName
+
+Publish-AzWebApp -ResourceGroupName $resourceGroupName -Name $functionName -ArchivePath $file -Verbose -Force
+
+Remove-Item $file -Force
